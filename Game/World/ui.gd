c3ab9cef718c01345c646b184b3fd200ca612c76
Paraldos <hillbrecht.s@gmail.com>
2023-08-22 23:27:19 +0200

    added credits to ui

--- Game/World/ui.gd
+++ Game/World/ui.gd
@@ -2,10 +2,17 @@ extends CanvasLayer
 
 @onready var health_top_bar = %HealthTopBar
 @onready var health_bottom_bar = %HealthBottomBar
+@onready var credits_label = %CreditsLabel
 
+############################################################
 func _ready():
 	Utils.change_hp.connect(_on_change_hp)
 
+############################################################
+func _physics_process(delta):
+	credits_label.text = 'Credits: %s' % Utils.player.credits
+
+############################################################
 func _on_change_hp():
 	health_top_bar.max_value = Utils.player.max_hp
 	health_bottom_bar.max_value = Utils.player.max_hp
