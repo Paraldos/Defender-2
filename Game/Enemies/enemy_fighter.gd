e616d754250be12d0fe2255dc0711f2bd79e1e13
Paraldos <hillbrecht.s@gmail.com>
2023-09-05 18:34:51 +0200

    reworked enemy_fighter

--- Game/Enemies/enemy_fighter.gd
+++ Game/Enemies/enemy_fighter.gd
@@ -1,12 +1,9 @@
 extends "res://Game/Enemies/enemy_template.gd"
 
 var direction = Vector2(-1.0, 1.0)
-var speed = 200
+var speed = 300
 var changing_direction = false
-
 @onready var movement_timer = %MovementTimer
-@onready var ray_cast_up = %RayCastUp
-@onready var ray_cast_down = %RayCastDown
 
 ###############################################################################
 func _ready():
@@ -21,26 +18,25 @@ func _process(delta):
 
 #########################
 func _movement(delta):
-	if ray_cast_up.is_colliding() && !changing_direction && direction.y != 1.0:
+	if global_position.y <= 50 && !changing_direction && direction.y != 1.0:
 		_change_direction(1.0)
 		movement_timer.start()
-	elif ray_cast_down.is_colliding() && !changing_direction && direction.y != -1.0:
+	elif global_position.y >= 230 && !changing_direction && direction.y != -1.0:
 		_change_direction(-1.0)
 		movement_timer.start()
-	position += Vector2(direction.x * speed, direction.y * speed / 2) * delta
+	position += Vector2(direction.x * speed, direction.y * speed / 3) * delta
 
 #########################
 func _on_movement_timer_timeout():
+	if changing_direction: return
 	_change_direction(direction.y * -1)
 	movement_timer.start(rng.randf_range(1, 3))
 
 #########################
 func _change_direction(y_direction):
 	var new_direction = Vector2(-1.0, y_direction)
-	var tween = get_tree().create_tween()
-	###
 	changing_direction = true
-	await tween.tween_property(
+	await create_tween().tween_property(
 		self,
 		'direction',
 		new_direction,
