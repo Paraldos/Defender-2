fd75dd3c4a1b3c23d386c9c7057d9b3f126950ef
Paraldos <hillbrecht.s@gmail.com>
2023-09-19 21:31:03 +0200

    reworked gunship projectile

--- Game/Enemies/GunShips/gunship.gd
+++ Game/Enemies/GunShips/gunship.gd
@@ -1,6 +1,7 @@
 extends "res://Game/Enemies/enemy_template.gd"
 
 @onready var muzzle = %Muzzle
+@onready var audio_stream_player_gun = $AudioStreamPlayerGun
 @export var speed = Vector2(-200, 0)
 var projectile = preload("res://Game/Projectile/gunship_projectile.tscn")
 
@@ -16,11 +17,12 @@ func _move(delta):
 ###############################################################################
 func _on_attack_timer_timeout():
 	for i in 3:
+		audio_stream_player_gun.play()
 		var x_speed = -250
 		var y_speed = 75
 		_spawn_projectile(Vector2(x_speed, y_speed))
 		_spawn_projectile(Vector2(x_speed, -y_speed))
-		await get_tree().create_timer(0.3).timeout
+		await get_tree().create_timer(0.4).timeout
 
 #########################
 func _spawn_projectile(movement):
