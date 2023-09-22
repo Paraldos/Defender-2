523d333ba6d54090694a051c94f4995981313770
Paraldos <hillbrecht.s@gmail.com>
2023-09-22 19:16:14 +0200

    WIP New Laser

--- Game/Weapons/Laser/laser.gd
+++ Game/Weapons/Laser/laser.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var audio_stream_player = $AudioStreamPlayer
+@export var active = false
 
 ##############################################################################
 func _ready():
